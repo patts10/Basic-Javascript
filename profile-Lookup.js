e2f28c582dfa0609fa6cab0c1c6699e0ec2593da
Jonathan Mendoza <patts2810@mail.com>
2020-12-05 22:32:41 -0500

    Initialize basic javascript with some exercises

---
+++ profile-Lookup.js
@@ -0,0 +1,71 @@
+//          Profile Lookup
+
+//We have an array of objects representing different people in our contacts lists.
+
+//A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+
+//The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+
+//If both are true, then return the "value" of that property.
+
+//If name does not correspond to any contacts then return "No such contact".
+
+//If prop does not correspond to any valid properties of a contact found to match name then return "No such property".
+
+
+// Setup
+var contacts = [
+  {
+      "firstName": "Akira",
+      "lastName": "Laine",
+      "number": "0543236543",
+      "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+      "firstName": "Harry",
+      "lastName": "Potter",
+      "number": "0994372684",
+      "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+      "firstName": "Sherlock",
+      "lastName": "Holmes",
+      "number": "0487345643",
+      "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+      "firstName": "Kristian",
+      "lastName": "Vos",
+      "number": "unknown",
+      "likes": ["JavaScript", "Gaming", "Foxes"]
+  }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+  for( let i = 0; i < contacts.length; i++ ) {
+    
+    if( contacts[i].firstName == name && contacts[i].hasOwnProperty(prop)) {
+      return contacts[i][prop];
+    } 
+    if( contacts[i].firstName == name && contacts[i].hasOwnProperty(prop) == false ) {  
+      return 'No such property';
+    }
+
+  }
+
+  for( let i = 0; i < contacts.length; i++ ) {
+    
+    if ( contacts[i].firstName != name) {
+      return'No such contact';
+    }
+
+  } 
+
+}
+
+console.log( lookUpProfile("Akira", "likes") );
+console.log( lookUpProfile("Bob", "number") );
+console.log( lookUpProfile("Kristian", "lastName") );
+console.log( lookUpProfile("Akira", "address") );
